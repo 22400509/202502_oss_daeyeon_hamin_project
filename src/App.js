f5cf75d1cd0243c4705d34a9643bae3295d87338
best <eodus0727@naver.com>
2025-10-16 09:49:44 +0900

    pull 땡기고 다시 커밋 10/16

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { use, useState } from 'react';
+import Color from './Component/Color';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p style={{ color: 'yellow' }}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
