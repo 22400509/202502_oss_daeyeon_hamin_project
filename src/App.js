aeb08970eeb3546eabb546383a723847e8404d58
KhaminK <hamin5y5@gmail.com>
2025-10-17 10:42:28 +0900

    tried to add App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import { use, useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DetailPage from './ProjectDetail';
 
 function App() {
   return (
-    <>
-      <DetailPage/>
-    </>
+    <Router>
+      <Routes>
+        {/* define the route with a dynamic nasaId param */}
+        <Route path="/detail/:nasaId" element={<DetailPage />} />
+
+        {/* (optional) default route */}
+        <Route path="/" element={<div style={{ color: 'white', textAlign: 'center' }}>Home Page</div>} />
+      </Routes>
+    </Router>
   );
 }
 
