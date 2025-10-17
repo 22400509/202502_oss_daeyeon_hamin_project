a9ef0fff70e4bf1b037dfd67b70640a74ac9dbd2
KhaminK <hamin5y5@gmail.com>
2025-10-17 10:14:03 +0900

    removed unnecessary imports in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import { use, useState } from 'react';
-// import Color from './Component/Color';
 import DetailPage from './ProjectDetail';
 
 function App() {
