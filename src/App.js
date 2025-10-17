5955d1f43f7b94695d61dd8be453e37309b0ab59
KhaminK <hamin5y5@gmail.com>
2025-10-17 10:59:46 +0900

    made MainPage files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { use, useState } from 'react';
 import DetailPage from './ProjectDetail';
+import MainPage from './mainPage/MainPage';
 
 function App() {
   return (
     <>
       <DetailPage/>
+      <MainPage/>
     </>
   );
 }
--- src/mainPage/MainPage.js
+++ src/mainPage/MainPage.js
@@ -2,21 +2,20 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './MainPage.css';
 
 function MainPage() {
     return (
         <>
-            <Container>
-                <Row className="align-items-end bg-danger" style={{ height: "30vw" }}>
-                    <Col sm={6}>
-                        <h1>AstroLens</h1>
-                        <h6>A new glimpse of the cosmos, delivered daily.<br/> This site features the official Astronomy Picture of the Day from NASA.</h6>
-                    </Col>
-                    <Col sm={6}>
-                        <input type="date" id="picturedate" name="user_picturedate"></input>
-                    </Col>
-                </Row>
-            </Container>
+            <div class="parallax-container">
+                <img id="foreground-img" src="z2 1.svg" alt="Astronaut in foreground"/>
+                    <h1 >NASA, Time to shine</h1>
+                    <a href="#bottom-section" id="scroll-btn">Explore</a>
+            </div>
+
+            <div id="bottom-section">
+                <p>You made it to the content!</p>
+            </div>
         </>
     );
 }
