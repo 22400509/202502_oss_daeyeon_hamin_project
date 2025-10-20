f8f7168ceec005a94c874a128ba5f3fe1561c3f3
KhaminK <hamin5y5@gmail.com>
2025-10-20 09:48:47 +0900

    styled search, filtering, and sort bar

--- src/mainPage/MainPage.js
+++ src/mainPage/MainPage.js
@@ -114,10 +114,10 @@ function MainPage() {
             <div id="bottom-section">
                 <div className="d-flex justify-content-evenly flex-wrap align-items-center gap-3 p-3">
                     <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
-                    <input type="date" value={selectedDate} onChange={(e) => setSelectedDate(e.target.value)} />
+                    <input type="date" className="date-input" value={selectedDate} onChange={(e) => setSelectedDate(e.target.value)} />
                     <div id="sort-container">
                         <label htmlFor="sort">Sort by:</label>
-                        <select id="sort" value={sortOrder} onChange={(e) => setSortOrder(e.target.value)}>
+                        <select className="sort-select" id="sort" value={sortOrder} onChange={(e) => setSortOrder(e.target.value)}>
                             <option value="date">Date</option>
                             <option value="alphabetical">A-Z</option>
                             <option value="reverse-alphabetical">Z-A</option>
--- src/mainPage/components/Search.js
+++ src/mainPage/components/Search.js
@@ -1,11 +1,13 @@
 // Search.js
 import React from "react";
+import './Search.css';
 
 // Receive props from MainPage
 function Search({ searchTerm, onSearchChange }) {
     return (
         <input
             type="search"
+            className="search-input"
             placeholder="Find your space"
             value={searchTerm} // Value comes from props
             onChange={(e) => onSearchChange(e.target.value)} // Call function from props
